e92d6abd489722219b2f258a36d7556a6b30480a
Nikita <nikita.ushackow.02@gmail.com>
2023-08-28 17:17:35 +0200

    docker: add working docker in server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -16,9 +16,9 @@ import {ExcursionPlaces} from "./places/excursion-places.model";
       SequelizeModule.forRoot({
           dialect: 'postgres',
           host: process.env.POSTGRES_HOST,
-          port: Number(process.env.POSTGRESS_PORT),
+          port: Number(process.env.POSTGRES_PORT),
           username: process.env.POSTGRES_USER,
-          password: process.env.POSTGRESS_PASSWORD,
+          password: process.env.POSTGRES_PASSWORD,
           database: process.env.POSTGRES_DB,
           models: [Excursion, Place, ExcursionPlaces],
           autoLoadModels: true
